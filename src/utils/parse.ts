e41434457fcc2898da7a4ca70bd71f00410cb77d
Stanley Horwood <stanley.horwood@hotmail.fr>
2021-08-11 22:42:51 +0000

    📝 Added context in error message

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,18 +1,16 @@
 
 //--- Import dependencies.
-import { parse as parseMeta } from '@metadata/parse'
+import { parse as parseMeta } from '@/metadata/parse'
 import { parse as parseYaml } from 'yaml'
 
 /**
- * Parse data from `json`, `yaml` or `xml`.
- * @param {string} string Data to parse.
- * @param {string} format Defines metadata file format.
- * @return {*} 
+ * Parse data from a `json`, `yaml` or `xml` formatted string.
+ * @param {string} string String to parse from.
+ * @param {string} format Format to parse from.
  */
-export function parse(data: string, format?: string): Object {
-         if(format === 'json')  return JSON.parse(data)
-    else if(format === 'yaml' || format === 'yml') return parseYaml(data)
-    else if(format === 'meta' || format === 'xml') return parseMeta(data)
-    else throw new Error('Invalid file format')
-    return {}
+export function parse(dataString: string, format?: string): Object {
+    if(format === 'json')  return JSON.parse(dataString)
+    else if(format === 'yaml' || format === 'yml') return parseYaml(dataString)
+    else if(format === 'meta' || format === 'xml') return parseMeta(dataString)
+    else throw new Error('Cannot parse string: Invalid file format.')
 }
--- src/utils/stringify.ts
+++ src/utils/stringify.ts
@@ -1,16 +1,16 @@
 
 //--- Import dependencies.
-import { stringify as stringifyMeta } from '@metadata/stringify'
+import { stringify as stringifyMeta } from '@/metadata/stringify'
 import { stringify as stringifyYaml } from 'yaml'
 
 /**
- * Strigify an object into a defined container.
- * @param {object} object Object to strigify.
- * @param {string} [format='json'] Format of the container.
+ * Strigify an object into a formatted string.
+ * @param {object} object Object to strigify from.
+ * @param {string} format Format to strigify to.
  */
 export function stringify(object: object, format = 'json' as string): string {
     if(format === 'json') return JSON.stringify(object, null, 2)
     else if(format === 'yaml' || format === 'yml') return stringifyYaml(object)
     else if(format === 'meta' || format === 'xml') return stringifyMeta(object)
-    else throw new Error('Invalid file format')
+    else throw new Error('Cannot strigify string: Invalid file format.')
 }
